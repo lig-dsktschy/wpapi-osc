bcc1d2ba62229f6f722c6ef2d6c8fc0ffb9d2de6
tsuchiya_daisuke <tsuchiya_daisuke@liginc.co.jp>
2016-11-04 02:17:58 +0900

    sample 2

--- 2/js/index.js
+++ 2/js/index.js
@@ -1,6 +1,21 @@
 'use strict';
-
-var ctx;
-
+var ctx, oscList, freqList;
+oscList = [];
+freqList = [
+    130.81,
+    261.63,
+    466.16,
+    783.99,
+    1174.66,
+    1396.91,
+    1760
+];
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 ctx = new AudioContext();
+freqList.forEach(function(freq, i) {
+    oscList.push(ctx.createOscillator());
+    oscList[i].type = 'sine';
+    oscList[i].frequency.value = freq;
+    oscList[i].connect(ctx.destination);
+    oscList[i].start(0);
+});
