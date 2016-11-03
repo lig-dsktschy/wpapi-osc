2172b48858b3aedfc0361faa46d49345dc4e9397
tsuchiya_daisuke <tsuchiya_daisuke@liginc.co.jp>
2016-11-04 01:45:13 +0900

    sample 1

--- 1/js/index.js
+++ 1/js/index.js
@@ -1,6 +1,9 @@
 'use strict';
-
-var ctx;
-
+var ctx, osc;
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 ctx = new AudioContext();
+osc = ctx.createOscillator();
+osc.type = 'sine';
+osc.frequency.value = 440;
+osc.connect(ctx.destination);
+osc.start(0);
